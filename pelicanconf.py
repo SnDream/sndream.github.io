c46a79e2ea10076468851a393de2b9a1d76d2ce0
sndream <sndream@outlook.com>
2024-07-07 02:23:51 +0800

    add site language

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,9 +8,23 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'zh_CN'
 
+DEFAULT_DATE_FORMAT = '%Y.%m.%d %A'
+DATE_FORMATS = {
+    'en': ("en_US.utf8", '%a, %d %b %Y'),
+    'zh_CN': ("zh_CN.utf8", '%Y.%m.%d %A'),
+}
+
+LOCALE = ("zh_CN.utf8", "en_US.utf8")
+
+ARTICLE_URL = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = '{category}/{slug}.html'
+ARTICLE_LANG_URL = '{category}/{slug}-{lang}.html'
+ARTICLE_LANG_SAVE_AS = '{category}/{slug}-{lang}.html'
+
 # Plugins
 PLUGIN_PATHS = ["plugins"]
-PLUGINS = ["filetime_from_git"]
+PLUGINS = ["filetime_from_git", "i18n_subsites", "auto_attach"]
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -62,7 +76,23 @@ GITHUB_CORNER_URL = "https://github.com/SnDream"
 SITELOGO = '/images/sitelogo.png'
 FAVICON = '/images/favicon.ico'
 
+USE_LESS = False
+
+OG_LOCALE = "zh_CN"
+
+ARTICLE_HIDE_TRANSLATION = False
+
 # FILETIME_FROM_GIT
 GIT_WARN_MODIFIED = True
 GIT_WARN_NOT_COMMITED = True
 GIT_WARN_NOT_MANAGED = True
+
+# I18N_SUBSITES
+I18N_SUBSITES = {
+    # Currently not working, disabled
+    # 'en': {
+    #     'SITENAME': 'GBZ80 Programming',
+    # },
+}
+
+I18N_TEMPLATES_LANG = "en"
